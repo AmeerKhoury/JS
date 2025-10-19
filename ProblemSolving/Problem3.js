32c13d461be8f039241d1f59d25727f91c1b1391
Ameer Majed Jamal Al-Khoury <khouryameer9@gmail.com>
2025-10-19 09:53:13 +0300

    Problem 3

---
+++ ProblemSolving/Problem3.js
@@ -0,0 +1,37 @@
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+}
+function getMinOfArray(numArray) {
+    return Math.min.apply(null, numArray);
+}
+function maximumProfit(array) {
+    if (array.length === 0) return 0;
+
+    let minPrice = array[0];
+
+    let maxProfit = 0;
+
+    for (let i = 1; i < array.length; i++) {
+        const current = array[i];
+
+        const profit = current - minPrice;
+
+        if (profit > maxProfit) {
+
+            maxProfit = profit;
+        }
+        if (current < minPrice) {
+
+            minPrice = current;
+        }
+    }
+    return maxProfit;
+}
+const price1 = [100, 80, 120, 130, 70, 60, 100, 125];
+
+const price2 = [100, 80, 70, 65, 60, 55, 50];
+
+console.log(maximumProfit(price1)); // Output: 65
+
+console.log(maximumProfit(price2)); // Output: 0
+export { };
---
+++ TypeScript/Problem3.ts
@@ -0,0 +1,35 @@
+function getMaxOfArray(numArray: number[]): number {
+  return Math.max(...numArray);
+}
+
+function getMinOfArray(numArray: number[]): number {
+  return Math.min(...numArray);
+}
+
+function maximumProfit(array: number[]): number {
+  if (!array || array.length === 0) return 0;
+
+  let minPrice: any = array[0];
+  let maxProfit: number = 0;
+
+  for (let i = 1; i < array.length; i++) {
+    const current: any = array[i];
+    const profit: number = current - minPrice;
+
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+
+    if (current < minPrice) {
+      minPrice = current;
+    }
+  }
+
+  return maxProfit;
+}
+
+const price1: number[] = [100, 80, 120, 130, 70, 60, 100, 125];
+const price2: number[] = [100, 80, 70, 65, 60, 55, 50];
+
+console.log(maximumProfit(price1)); // Output: 65
+console.log(maximumProfit(price2)); // Output: 0
